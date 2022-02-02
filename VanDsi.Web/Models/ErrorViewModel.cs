dc0693cb15c6e1f97a7a27f128a9f00c78b688d0
Sercan Sayin <sercan.sayin@ericsson.com>
2022-02-03 00:33:51 +0300

    Add project files.

---
+++ VanDsi.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VanDsi.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
